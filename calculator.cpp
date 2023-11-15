281139fdc61e97016e5ecab91be2e7b1befe291a
Trajil <trajil@web.de>
2023-11-15 16:26:32 +0100

    added while loop for exit

--- calculator.cpp
+++ calculator.cpp
@@ -3,10 +3,8 @@ using  std::cin, std::cout, std::string;
 
 /* CURRENT TASK:
 
-"Using the Basic/A/Problem 1 project make a control structure using if statements and logical operators. 
-The structure should give the user a choice between the available arithmetic operations. The choice should be the sign of the operation"
-
-    
+    "Using the Control structures/B/Problem 1 project and add repetability to the project. Choose the right loop to give the user as many arithmetic operations as he/she wishes. 
+    All the code should be in the loop. Write a few sentances to elaborate your loop choice"
 
 */
 
@@ -16,49 +14,57 @@ int main()
     //fixed outputs
     string t_welcome_1 = "Hi there, buddy! We're doing maths and stuff! First: Enter your two numbers: \n";
     string t_sum="The sum is: ";
-    string t_dif="\nThe difference is: ";
-    string t_product="\nThe product is: ";
-    string t_quo="\nThe quotient is: ";
+    string t_dif="The difference is: ";
+    string t_product="The product is: ";
+    string t_quo="The quotient is: ";
     string t_nunbers_wrong = "Please only use whole numbers!";
 
      
     //choices
     string t_choice = "Now select the type of arithmetic operation (+ - / or *) you want to use: "; 
     string t_choice_wrong = "Please use: (+ - / or *)";
+    string t_exit_question = "\nPress 'Y' to repeat!";
+    string t_end = "\nGood bye!";
 
 
 
     double x, y;
-    char choice_operator;
-    
+    char choice_operator, exit = 'Y';
 
-    cout << t_welcome_1; cin >> x >> y; cout << t_choice; cin >> choice_operator;
-    
-    double sum= x + y, dif= x - y, product= x * y, quo= x / y;
-
-
-    switch(choice_operator)
+    while( exit == 'y' || exit == 'Y')
     {
-        case '+': 
-            cout << t_sum << sum;
-            break;
+        cout << t_welcome_1; cin >> x >> y; cout << t_choice; cin >> choice_operator;
 
-        case '-':
-            cout << t_dif << dif;
-            break;
+        double sum= x + y, dif= x - y, product= x * y, quo= x / y;
 
-        case '/':
-            cout << t_quo << quo;
-            break;
 
-        case '*':
-            cout << t_product << product;
-            break;
+        switch(choice_operator)
+        {
+            case '+': 
+                cout << t_sum << sum;
+                break;
 
-        default:
-            cout << t_choice_wrong;
-            break;
+            case '-':
+                cout << t_dif << dif;
+                break;
+
+            case '/':
+                cout << t_quo << quo;
+                break;
+
+            case '*':
+                cout << t_product << product;
+                break;
+
+            default:
+                cout << t_choice_wrong;
+                break;
+        }
+
+    cout << t_exit_question; cin >> exit;
     }
+
+    cout << t_end ;
 
     return 0;
 
