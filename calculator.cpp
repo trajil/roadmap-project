b92f10a5fffdded84e6a1fa21d008516de7ae72c
Trajil <trajil@web.de>
2023-12-05 01:05:14 +0100

    refactoring of question order

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,29 @@ using std::cin, std::cout, std::string, std::vector;
 
 /* CURRENT TASK:
 
+    REFACTORING:
+        Change the user input order to:
+        1. Welcome Screen
+        2. Question of about methods:
+            - Arithmetics
+            - Translation to Numerical System with base N
+        if Arithmetics:
+            Question: INT OR FLOAT?
+            -> Input asked: I/F
+            Question: OPERATOR (+ - * / P)
+            -> Input asked:
+            2 Numbers: ?
+            MATH-STUFF...
+            Question: Repeat?
+            if yes: -> STEP 2
+        if Translation:
+            -> Input asked:
+            System with Base to: ?
+            -> Input asked:
+            Decimal number to convert: ?
+            MATH-STUFF...
+            Question: Repeat?
+            if yes: -> STEP 2
 */
 
 // outputs
@@ -19,17 +42,19 @@ string t_datatype_output_double = " (FLOAT)";
 string t_numbers_wrong = "Please only use whole numbers!\n";
 string t_datatype_wrong = "Please only use 'F' or 'I'! \n";
 
+string filler = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+
 // inputs
-string t_input_operator = "First, enter the operator (+, -, /, * or p): ";
-string t_input_numbers_i = "Now type in your two numbers (only whole numbers!)\n";
-string t_input_numbers_f = "Now type in your two numbers! \n";
-string t_input_numbers_dec = "Translate decimal [NUMB1] to system_with_base_n[NUMB2] - POSITIVE INTs ONLY!\n";
+string t_input_operation_kind = "Choose the kind of operation you wanna do:\n(1) ARITHMETICS\n(2) TRANSLATION of a dec to another base\noperation: ";
+string t_input_operator = "Choose your operator: (+, -, /, * or p)\noperator: ";
+string t_input_numbers_int = "Now type in your two numbers (only whole numbers!)\n";
+string t_input_numbers_float = "Now type in your two numbers!\n";
 string t_input_numbers_1 = "Number 1: ";
 string t_input_numbers_2 = "Number 2: ";
-string t_choice_wrong = "Please use: (+, -, /, * or p)";
+string t_choice_wrong = "Please use: (+, -, /, * or p)\n";
 string t_exit_question = "\nPress 'Y' to repeat!";
 string t_end = "\nGood bye!";
-string t_input_datatype = "Calculations [INT:I] or [FLOAT:F], Translations [DEC:D]? ";
+string t_input_datatype = "Calculations with [INT:I] or [FLOAT:F]?\ndata_type: ";
 
 int x, y;
 double a, b;
@@ -49,133 +74,140 @@ double power(double a, double b);
 
 void dec_to_base_X_numeral_system(int y, int x);
 
-void control(char choice_operator = '+', char choice_datatype = 'I')
+void control(char choice_operation_kind = '1')
 {
-    // FLOAT VALUES
+    char choice_datatype, choice_operator;
 
-    if (choice_datatype == 'F' || choice_datatype == 'f')
+    switch (choice_operation_kind)
     {
-        cout << t_input_numbers_f << t_input_numbers_1;
-        cin >> a;
-        cout << t_input_numbers_2;
-        cin >> b;
-        switch (choice_operator)
-        {
-        case '+':
-            cout << t_sum << sum(a, b) << t_datatype_output_double;
-            break;
+    case '1':
+        cout << t_input_operator;
+        cin >> choice_operator;
+        break;
 
-        case '-':
-            cout << t_dif << dif(a, b) << t_datatype_output_double;
-            break;
-
-        case '/':
-            cout << t_quo << quo(a, b) << t_datatype_output_double;
-            break;
-
-        case '*':
-            cout << t_product << product(a, b) << t_datatype_output_double;
-            break;
-        case 'p':
-            cout << a << t_power << b << " is: " << power(a, b) << t_datatype_output_double;
-            break;
-
-        default:
-            cout << t_choice_wrong;
-            break;
-        }
-    }
-
-    // INT VALUES
-    else if (choice_datatype == 'I' || choice_datatype == 'i')
-    {
-        cout << t_input_numbers_i << t_input_numbers_1;
-        cin >> x;
-        cout << t_input_numbers_2;
+    case '2':
+        cout << "Input decimal number: \n";
         cin >> y;
-        switch (choice_operator)
-        {
-        case '+':
-            cout << t_sum << sum(x, y) << t_datatype_output_int;
-            break;
-
-        case '-':
-            cout << t_dif << dif(x, y) << t_datatype_output_int;
-            break;
-
-        case '/':
-            cout << t_quo << quo(x, y) << t_datatype_output_int;
-            break;
-
-        case '*':
-            cout << t_product << product(x, y) << t_datatype_output_int;
-            break;
-
-        default:
-            cout << t_choice_wrong;
-            break;
-        }
-    }
-
-    else if (choice_datatype == 'D' || choice_datatype == 'd')
-    {
-        cout << t_input_numbers_dec << t_input_numbers_1;
+        cout << "Input numerical system base: \n";
         cin >> x;
-        cout << t_input_numbers_2;
-        cin >> y;
-        dec_to_base_X_numeral_system(x, y);
-        cout << "\nYour number in the system with base: " << x << " is:\n";
+        dec_to_baseX.clear();
+        dec_to_base_X_numeral_system(y, x);
+        cout << "\nYour number in the system with base_" << x << " is:\n";
         for (int i : dec_to_baseX)
         {
             cout << i;
         }
+        return;
+        break;
+
+    default:
+        break;
     }
-    else
+
+    switch (choice_operator)
     {
-        cout << t_datatype_wrong << std::endl;
-    }
-}
-
-int main()
-{
-
-    char choice_datatype, exit;
-    char choice_operator;
-    string input;
-
-    cout << t_welcome << std::endl;
-    do
-    {
-        choice_operator = 0;
-        cout << t_input_operator;
-        getline(cin, input); // only way for the input to accept [ENTER]
-
-        if (input.empty())
-        {
-            cout << "(input left empty)\n";
-        }
-        else
-        {
-            for (int i = 0; i < input.length(); i++) // Getting the last digit of the string to char -by Victor
-            {
-                choice_operator = input[i];
-            }
-        }
-
+    case '+':
         cout << t_input_datatype;
         cin >> choice_datatype;
-
-        char i = choice_operator;
-
-        if (i == 0) // how can I accept an empty input? (solved)
+        if (choice_datatype == 'F' || choice_datatype == 'f')
         {
-            cout << "\n VALUE 0\n";
-            control();
+            cout << t_input_numbers_float << t_input_numbers_1;
+            cin >> a;
+            cout << t_input_numbers_2;
+            cin >> b;
+            cout << t_sum << sum(a, b) << t_datatype_output_double;
         }
+        else if (choice_datatype == 'I' || choice_datatype == 'i')
+        {
+            cout << t_input_numbers_int << t_input_numbers_1;
+            cin >> x;
+            cout << t_input_numbers_2;
+            cin >> y;
+            cout << t_sum << sum(x, y) << t_datatype_output_int;
+        }
+        break;
 
+    case '-':
+        cout << t_input_datatype;
+        cin >> choice_datatype;
+        if (choice_datatype == 'F' || choice_datatype == 'f')
+        {
+            cout << t_input_numbers_float << t_input_numbers_1;
+            cin >> a;
+            cout << t_input_numbers_2;
+            cin >> b;
+            cout << t_sum << dif(a, b) << t_datatype_output_double;
+        }
+        else if (choice_datatype == 'I' || choice_datatype == 'i')
+        {
+            cout << t_input_numbers_int << t_input_numbers_1;
+            cin >> x;
+            cout << t_input_numbers_2;
+            cin >> y;
+            cout << t_sum << dif(x, y) << t_datatype_output_int;
+        }
+        break;
+
+    case '/':
+        cout << t_input_numbers_float << t_input_numbers_1;
+        cin >> a;
+        cout << t_input_numbers_2;
+        cin >> b;
+        cout << t_sum << quo(a, b) << t_datatype_output_double;
+
+        break;
+
+    case '*':
+        cout << t_input_datatype;
+        cin >> choice_datatype;
+        if (choice_datatype == 'F' || choice_datatype == 'f')
+        {
+            cout << t_input_numbers_float << t_input_numbers_1;
+            cin >> a;
+            cout << t_input_numbers_2;
+            cin >> b;
+            cout << t_sum << product(a, b) << t_datatype_output_double;
+        }
+        else if (choice_datatype == 'I' || choice_datatype == 'i')
+        {
+            cout << t_input_numbers_int << t_input_numbers_1;
+            cin >> x;
+            cout << t_input_numbers_2;
+            cin >> y;
+            cout << t_sum << product(x, y) << t_datatype_output_int;
+        }
+        break;
+    case 'p':
+        cout << "You chose the power function. Please input your number: ";
+        cin >> a;
+        cout << "To the power of: ";
+        cin >> b;
+        cout << a << t_power << b << " is: " << power(a, b) << t_datatype_output_double;
+        break;
+
+    default:
+        cout << t_choice_wrong;
+        break;
+    }
+}
+int main()
+{
+    char choice_operation_kind, exit;
+
+    cout << t_welcome << std::endl;
+
+    do
+    {
+        cout << filler << t_input_operation_kind;
+        cin >> choice_operation_kind;
+
+        if (int i = choice_operation_kind; i == '1' || i == '2')
+        {
+            control(choice_operation_kind);
+        }
         else
         {
-            control(choice_operator, choice_datatype);
+            cout << "Please only use one of the options above!\n";
         }
 
         cout << t_exit_question;
@@ -258,8 +290,13 @@ void dec_to_base_X_numeral_system(int y, int x)
     y = abs(y);
     x = abs(x);
 
+    if (x < 2)
+    {
+        cout << "\n!!!Base too low!!!\n";
+        return;
+    }
+
     int rest;
-    dec_to_baseX.clear();
 
     // 'Y' is the integer, 'X' is the base_system (e.g. x=2 is binary system)
     if (y >= x)
